cd3813e7afa260fb5adb72386044f9c324c36de1
sreekanth1100 <40232293+sreekanth1100@users.noreply.github.com>
2023-04-25 23:19:13 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -6,22 +6,22 @@ set -o nounset
 IFS=$(printf '\n\t')
 
 # Docker
-sudo apt remove --yes docker docker-engine docker.io containerd runc || true
-sudo apt update
-sudo apt --yes --no-install-recommends install apt-transport-https ca-certificates
+apt remove --yes docker docker-engine docker.io containerd runc || true
+apt update
+apt --yes --no-install-recommends install apt-transport-https ca-certificates
 wget --quiet --output-document=- https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository --yes "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release --codename --short) stable"
-sudo apt update
-sudo apt --yes --no-install-recommends install docker-ce docker-ce-cli containerd.io
-sudo usermod --append --groups docker "$USER"
-sudo systemctl enable docker
+add-apt-repository --yes "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release --codename --short) stable"
+apt update
+apt --yes --no-install-recommends install docker-ce docker-ce-cli containerd.io
+usermod --append --groups docker "$USER"
+systemctl enable docker
 printf '\nDocker installed successfully\n\n'
 
 printf 'Waiting for Docker to start...\n\n'
 sleep 5
 
 # Docker Compose
-sudo wget --output-document=/usr/local/bin/docker-compose "https://github.com/docker/compose/releases/download/$(wget --quiet --output-document=- https://api.github.com/repos/docker/compose/releases/latest | grep --perl-regexp --only-matching '"tag_name": "\K.*?(?=")')/run.sh"
-sudo chmod +x /usr/local/bin/docker-compose
-sudo wget --output-document=/etc/bash_completion.d/docker-compose "https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose"
+wget --output-document=/usr/local/bin/docker-compose "https://github.com/docker/compose/releases/download/$(wget --quiet --output-document=- https://api.github.com/repos/docker/compose/releases/latest | grep --perl-regexp --only-matching '"tag_name": "\K.*?(?=")')/run.sh"
+chmod +x /usr/local/bin/docker-compose
+wget --output-document=/etc/bash_completion.d/docker-compose "https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose"
 printf '\nDocker Compose installed successfully\n\n'
